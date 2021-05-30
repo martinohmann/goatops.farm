9c5dda5761decd0cc3b4d4f8b3c46c89c9c5333e
martinohmann <martinohmann@gmail.com>
2021-05-30 11:56:48 +0200

    add goa design

---
+++ design/api.go
@@ -0,0 +1,80 @@
+package design
+
+import . "goa.design/goa/v3/dsl"
+
+var _ = API("goatfacts", func() {
+	Title("Goat facts Service")
+	Description("Service for obtaining your daily dose of goat facts")
+	Server("goatfacts", func() {
+		Host("localhost", func() {
+			URI("http://localhost:8080")
+		})
+		Host("goatops.farm", func() {
+			Description("public host")
+			URI("https://goatops.farm")
+		})
+	})
+})
+
+var _ = Service("goatfacts", func() {
+	Description("The goatfacts service provides you with important facts about goats.")
+
+	Method("get-fact", func() {
+		Payload(func() {
+			Field(1, "id", String, "ID of the fact")
+			Required("id")
+		})
+
+		Result(Fact)
+
+		Error("NotFound")
+		Error("BadRequest")
+
+		HTTP(func() {
+			GET("/facts/{id}")
+			Response(StatusOK)
+			Response("NotFound", StatusNotFound)
+			Response("BadRequest", StatusBadRequest)
+		})
+	})
+
+	Method("list-facts", func() {
+		Result(ArrayOf(Fact))
+
+		HTTP(func() {
+			GET("/facts")
+			Response(StatusOK)
+		})
+	})
+
+	Method("get-random-fact", func() {
+		Result(Fact)
+
+		Error("NotFound")
+
+		HTTP(func() {
+			GET("/facts/random")
+			Response(StatusOK)
+			Response("NotFound", StatusNotFound)
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+	Files("/openapi.yaml", "./gen/http/openapi.yaml")
+})
+
+var Fact = Type("Fact", func() {
+	Description("A fact about goats.")
+	TypeName("Fact")
+
+	Attribute("id", String, "A unique ID", func() {
+		Example("123abc")
+		MinLength(1)
+		MaxLength(255)
+	})
+	Attribute("text", String, "Fact text", func() {
+		Example("Goats will not rick-roll you")
+	})
+
+	Required("id", "text")
+})
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/martinohmann/goatops.farm
 
 go 1.16
 
-require github.com/microcosm-cc/bluemonday v1.0.9
+require (
+	github.com/microcosm-cc/bluemonday v1.0.9
+	goa.design/goa/v3 v3.4.2
+)
